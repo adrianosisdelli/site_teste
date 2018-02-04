2bd5d63c63b1a415df8ef0fc570ce8a882fa5893
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-02-04 16:11:57 -0200

    Implementação da classe Xml Generator (em funcionamento)

--- js/jscript.js
+++ js/jscript.js
@@ -1,9 +1,10 @@
-function Elemento(element, qtd){
+function Elemento(element, qtd, xml){
 	var el = element;
 	var elqtde = qtd;
 	var boldText = "Atenção! ";
 	var normalText = "Isto acaba de ser adicionado ao clicar no botão";
 	var qtde = 0;
+	var generator = new XmlGenerator("alert-box", xml);
 
 	this.setWarning = function(){
 		el.removeClass();
@@ -29,9 +30,24 @@ function Elemento(element, qtd){
 		});
 
 		qtde++;
-
-		box.html("<strong>Atenção: </strong>Mensagem incorreta. Quantidade: <strong>" + qtde + "</strong>");
+		var text = "<strong>Atenção: </strong>Mensagem incorreta. Quantidade: <strong>" + qtde + "</strong>";
+		box.html(text);
 		elem.prepend(box);
 		elqtde.html(qtde);
+		generator.generateNewElement(text);
+	}
+}
+
+function XmlGenerator(vElementName, vDisplay){
+	var indice = 0;
+	var display = vDisplay;
+	var elementName = vElementName;
+	var generated = [];
+
+	this.generateNewElement = function(value){
+		var elementText = "<pre><" + elementName + ">" + value + "</" + elementName + "></pre>";
+		indice++;
+		generated.push(elementText);
+		display.prepend(elementText);
 	}
 }
