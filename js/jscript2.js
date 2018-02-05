1eb01110c45bfff0e520fe16ec4c5eeaccaad69f
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-02-05 13:14:00 -0200

    Adição de funcionalidade na classe Conta: geração de extrato (log) interno no ato do saque

--- js/jscript2.js
+++ js/jscript2.js
@@ -1,8 +1,12 @@
 function Conta(vSaldo){
 	var saldo = vSaldo;
+	var extrato = [];
 
 	this.saque = function(vSaque){
 		if(vSaque <= saldo){
+			var dataHora = new Date();
+			var texto = dataHora.toLocaleString() + " - Movimentação no valor de R$ " + vSaque.toFixed(2);
+			extrato.push(texto);
 			saldo -= vSaque;
 			return vSaque;
 		}
